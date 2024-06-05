213b27f2cf5574d9a69fd9dacb8178395b16caa7
RezaAditiyaa <rzaditiya@students.amikom.ac.id>
2024-06-05 17:44:28 +0700

    add models

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,8 @@ use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class User extends Authenticatable
 {
     use HasApiTokens;
@@ -19,6 +21,15 @@ class User extends Authenticatable
     use HasTeams;
     use Notifiable;
     use TwoFactorAuthenticatable;
+
+    use SoftDeletes;
+
+    protected $dates = [
+        'updated_at',
+        'created_at',
+        'deleted_at',
+        'email_verified_at'
+    ];
 
     /**
      * The attributes that are mass assignable.
@@ -58,4 +69,30 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+
+
+    // one to one
+    public function detail_user()
+    {
+        return $this->hasOne('App\Models\DetailUser', 'users_id');
+    }
+
+
+    // one to many
+    public function service()
+    {
+        return $this->hasMany('App\Models\Service', 'users_id');
+    }
+
+    public function order_buyer()
+    {
+        return $this->hasMany('App\Models\Order', 'buyer_id');
+    }
+
+    public function order_freelancer()
+    {
+        return $this->hasMany('App\Models\Order', 'freelancer_id');
+    }
+
 }
