ae03991440549a2ef3be8ff7b26c59864bd085f8
wilgotenk <wildanamru123@gmail.com>
2024-07-11 19:53:01 +0700

    add explore page

--- app/Http/Controllers/Landing/LandingController.php
+++ app/Http/Controllers/Landing/LandingController.php
@@ -5,7 +5,12 @@ namespace App\Http\Controllers\Landing;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Order;
 use App\Models\Service;
+use App\Models\AdvantageUser;
+use App\Models\Tagline;
+use App\Models\AdvantageService;
+use App\Models\ThumbnailService;
 
 class LandingController extends Controller
 {
@@ -85,5 +90,59 @@ class LandingController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    // custom
+
+    public function explore()
+    {
+        $services = Service::orderBy('created_at', 'desc')->get();
+
+        return view('pages.landing.explore', compact('services'));
+    }
+
+    public function detail($id)
+    {
+        $service = Service::where('id', $id)->first();
+
+        $thumbnail = ThumbnailService::where('service_id', $id)->get();
+        $advantage_user = AdvantageUser::where('service_id', $id)->get();
+        $advantage_service = AdvantageService::where('service_id', $id)->get();
+        $tagline = Tagline::where('service_id', $id)->get();
+
+        return view('pages.landing.detail', compact('service', 'thumbnail', 'advantage_user', 'advantage_service', 'tagline'));
+    }
+
+    public function booking($id)
+    {
+        $service = Service::where('id', $id)->first();
+        $user_buyer = Auth::user()->id;
+
+        // validation booking
+        if ($service->users_id == $user_buyer) {
+            toast()->warning('Sorry, members cannot book their own service!');
+            return back();
+        }
+
+        $order = new Order;
+        $order->buyer_id = $user_buyer;
+        $order->freelancer_id = $service->user->id;
+        $order->service_id = $service->id;
+        $order->file = NULL;
+        $order->note = NULL;
+        $order->expired = Date('y-m-d', strtotime('+3 days'));
+        $order->order_status_id = 4;
+        $order->save();
+
+        $order_detail = Order::where('id', $order->id)->first();
+
+        return redirect()->route('detail.booking.landing', $order->id);
+    }
+
+    public function detail_booking($id)
+    {
+        $order = Order::where('id', $id)->first();
+
+        return view('pages.landing.booking', compact('order'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ use App\Http\Controllers\Dashboard\ServiceController;
 |
 */
 
+Route::get('detail_booking/{id}', [LandingController::class, 'detail_booking'])->name('detail.booking.landing');
+Route::get('booking/{id}', [LandingController::class, 'booking'])->name('booking.landing');
+Route::get('detail/{id}', [LandingController::class, 'detail'])->name('detail.landing');
+Route::get('explore', [LandingController::class, 'explore'])->name('explore.landing');
 Route::resource('/', LandingController::class);
 
 Route::group(['prefix' => 'member', 'as' => 'member.', 'middleware' => ['auth:sanctum', 'verified']], function () {
